9f1d88d86a0b7ab5c4eec87786b9ea54135813f1
Tobiasz Jarosiewicz <tobiasz.jarosiewicz@gmail.com>
2021-08-06 11:35:34 +0200

    Add script for handling lock screen keyboard shortcut.

---
+++ lock_screen.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+revert() {
+  rm /tmp/*screen*.png
+  xset dpms 0 0 0
+}
+trap revert HUP INT TERM
+xset +dpms dpms 0 0 5
+import -delay 1 -window root /tmp/locking_screen_orig.png
+convert -gamma 0.7 /tmp/locking_screen_orig.png /tmp/locking_screen.png
+convert -blur 0x8 /tmp/locking_screen.png /tmp/screen_blur.png
+convert -composite /tmp/screen_blur.png /home/tob/skrypty/custom_image.png -gravity Center -geometry -20x1200 /tmp/screen.png
+i3lock -i /tmp/screen.png
+revert
